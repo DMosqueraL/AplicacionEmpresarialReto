70baf415a35fb70d6515988a492c3b5a88d36055
DMosqueral <dml2410@gmail.com>
2022-06-13 05:55:33 -0500

    ADD --- Se agrega nuevamente la sincronización de los Endpoints con Swagger 3.0

--- src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
+++ src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
@@ -3,6 +3,12 @@ package co.com.sofka.questions.routers;
 import co.com.sofka.questions.model.AnswerDTO;
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.usecases.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +29,9 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class QuestionRouter {
 
     @Bean
-    @RouterOperation(beanClass = ListUseCase.class, beanMethod = "get")
+    @RouterOperation(beanClass = ListUseCase.class, beanMethod = "get",
+            operation = @Operation(operationId = "getAllQuestions",
+                    summary = "Listado de Preguntas", tags = {"Preguntas"}))
     public RouterFunction<ServerResponse> getAll(ListUseCase listUseCase) {
         return route(GET("/getAll"),
                 request -> ServerResponse.ok()
@@ -33,7 +41,12 @@ public class QuestionRouter {
     }
 
     @Bean
-    @RouterOperation(beanClass = OwnerListUseCase.class, beanMethod = "apply")
+    @RouterOperation(operation = @Operation(operationId = "getOwnerAll",
+            summary = "Obtener preguntas por Id del usuario", tags = {"Preguntas"},
+            parameters = {@Parameter(in = ParameterIn.PATH, name = "userId", description = "UserId")},
+            responses = {@ApiResponse(responseCode = "200", description = "Operación exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Id de usuario inválido"),
+                    @ApiResponse(responseCode = "404", description = "Id de usuario no encontrado")}))
     public RouterFunction<ServerResponse> getOwnerAll(OwnerListUseCase ownerListUseCase) {
         return route(
                 GET("/getOwnerAll/{userId}"),
@@ -47,7 +60,9 @@ public class QuestionRouter {
     }
 
     @Bean
-    @RouterOperation(beanClass = CreateUseCase.class, beanMethod = "apply")
+    @RouterOperation(beanClass = CreateUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "createQuestion",
+                    summary = "Crear Pregunta",  tags = {"Preguntas"}))
     public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
         Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO ->  createUseCase.apply(questionDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -61,7 +76,12 @@ public class QuestionRouter {
     }
 
     @Bean
-    @RouterOperation(beanClass = GetUseCase.class, beanMethod = "apply")
+    @RouterOperation(operation = @Operation(operationId = "QuestionDTO",
+            summary = "Obtener una pregunta por ID", tags = {"Preguntas"},
+            parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "id")},
+            responses = {@ApiResponse(responseCode = "200", description = "Operación exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Id de pregunta inválido"),
+                    @ApiResponse(responseCode = "404", description = "Pregunta no encontrada")}))
     public RouterFunction<ServerResponse> get(GetUseCase getUseCase) {
         return route(
                 GET("/get/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -75,7 +95,9 @@ public class QuestionRouter {
     }
 
     @Bean
-    @RouterOperation(beanClass = AddAnswerUseCase.class, beanMethod = "apply")
+    @RouterOperation(beanClass = AddAnswerUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "addAnswer",
+                    summary = "Agregar Respuesta",  tags = {"Respuestas"}))
     public RouterFunction<ServerResponse> addAnswer(AddAnswerUseCase addAnswerUseCase) {
         return route(POST("/add").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(AnswerDTO.class)
@@ -88,7 +110,12 @@ public class QuestionRouter {
     }
 
     @Bean
-    @RouterOperation(beanClass = DeleteUseCase.class, beanMethod = "apply")
+    @RouterOperation(operation = @Operation(operationId = "deleteQuestionByID",
+            summary = "Borrar pregunta por ID", tags = {"Preguntas"},
+            parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Question Id")},
+            responses = {@ApiResponse(responseCode = "202", description = "Operación exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Id de pregunta inválido"),
+                    @ApiResponse(responseCode = "404", description = "Pregunta no encontrada")}))
     public RouterFunction<ServerResponse> delete(DeleteUseCase deleteUseCase) {
         return route(
                 DELETE("/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
